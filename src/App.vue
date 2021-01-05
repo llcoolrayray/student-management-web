acd98aec0362ede780a052178491196704a869ce
754165369@qq.com <754165369@qq.com>
2021-01-05 22:20:45 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  html,body,#root{
+    width: 100%;
+    height: 100%;
+  }
+  .ant-layout{
+    height: 100%;
+  }
+
+  #app {
+    height: 100%;
+  }
+</style>
